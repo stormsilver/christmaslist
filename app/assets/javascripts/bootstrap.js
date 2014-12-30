9d22f308c21dd1e0658f95bf9e820f30448d54e2
Eric Hankins <ehankins@rednovalabs.com>
2014-12-30 11:46:16 -0600

    boottrap select

--- app/assets/javascripts/bootstrap.js
+++ app/assets/javascripts/bootstrap.js
@@ -2,3 +2,8 @@ jQuery(function() {
   // $("a[rel~=popover], .has-popover").popover();
   $('a[rel~=tooltip], .has-tooltip, [data-toggle="tooltip"]').tooltip();
 });
+
+$.fn.selectpicker.defaults = {
+    iconBase: 'fa',
+    tickIcon: 'fa-check'
+};
