244af1bc5454ecf1fb603487cc5ee849121c5ae2
Eric Hankins <ehankins@rednovalabs.com>
2015-11-04 22:36:23 -0600

    fix configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,8 +59,8 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.asset_host = "http://http://eric-christmas-list.herokuapp.com"
-  config.action_mailer.default_url_options = { host: "http://http://eric-christmas-list.herokuapp.com" }
+  config.action_mailer.asset_host = "http://eric-christmas-list.herokuapp.com"
+  config.action_mailer.default_url_options = { host: "http://eric-christmas-list.herokuapp.com" }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
